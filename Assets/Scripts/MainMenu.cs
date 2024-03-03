6a94e4b71fde94cc370f9cfe213dcc5d87431c97
Parven <parvendranofficial05@gmail.com>
2024-03-03 22:22:31 +0800

    added intro cutscene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,21 +7,32 @@ public class MainMenu : MonoBehaviour
 	[Header("Panel")]
 	[SerializeField] private GameObject panel; // Panel game object
 	
+	[Header("Mission Text")]
+	[SerializeField] private GameObject missionText;
+	
 	[Header("Panel Delay Time")]
 	[SerializeField] private float delayPanelTime; // Panel delay => pause "several time" after change to black
+	
+	[Header("Mission Text Pause Time")]
+	[SerializeField] private float missionTextDuration;
 
 	private Animator panelAnim;
+	private Animator missionTextAnim;
 	
 	private void Start()
 	{
 		panel.SetActive(false);
 		panelAnim = panel.GetComponent<Animator>(); // Get the animator component
+		
+		missionText.SetActive(false);
+		missionTextAnim = missionText.GetComponent<Animator>();
 	}
 
 	public void LoadScene() // Load scene to game scene
 	
 	{	
 		PlayPanelAnimation();
+		StartCoroutine(MissionTextDuration());
 		StartCoroutine(DelayPanelAnim());
 	}
 	
@@ -44,5 +55,15 @@ public class MainMenu : MonoBehaviour
 	{
 		yield return new WaitForSeconds(delayPanelTime);
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+
+	public IEnumerator MissionTextDuration()
+	{
+		missionText.SetActive(true);
+		yield return new WaitForSeconds(2.5f);
+		missionTextAnim.SetBool("isFade", true);
+		yield return new WaitForSeconds(missionTextDuration);
+		missionTextAnim.SetBool("isFade", false);
 	}
 }
