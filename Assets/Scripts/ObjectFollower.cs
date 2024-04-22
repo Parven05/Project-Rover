29837ffa41e10b90cd6252784121e6e710add7b3
Mohammed Musthafa <mohamedmusthafa3679@gmail.com>
2024-04-22 19:40:29 +0400

    //

--- Assets/Scripts/ObjectFollower.cs
+++ Assets/Scripts/ObjectFollower.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class ObjectFollower : MonoBehaviour
 {
-    [SerializeField] private Transform targetObject;
+    [SerializeField] private Transform targetObjectTransform;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float objectMoveSpeed = 10f;
-
     private void LateUpdate()
     {
-        if(targetObject)
-        transform.position = Vector3.Lerp(transform.position, targetObject.position + offset, Time.deltaTime * objectMoveSpeed);
+        transform.position = Vector3.Lerp(transform.position, targetObjectTransform.position + offset, Time.deltaTime * objectMoveSpeed);
     }
 }
