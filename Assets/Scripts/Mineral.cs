5ca2525f7ee0d915e035d42570856c6949287b49
Parven <parvendranofficial05@gmail.com>
2024-05-06 16:34:50 +0800

    added dissolve effect to minerals

--- Assets/Scripts/Mineral.cs
+++ Assets/Scripts/Mineral.cs
@@ -1,5 +1,5 @@
 using StarterAssets;
-using System;
+using System.Collections;
 using UnityEngine;
 
 public class Mineral : MonoBehaviour
@@ -8,9 +8,16 @@ public class Mineral : MonoBehaviour
     [Space]
     [SerializeField] private GameObject dustParticleGo;
     [SerializeField] private GameObject gloomParticleGo;
-    [SerializeField] private ParticleSystem pickUpParticleEffect;
+    [SerializeField] private Material dissolveMaterial;
+
     [Space]
     [SerializeField] private float mineralDestroyDelay = 1f;
+
+    private bool isDissolving = false;
+    private float dissolveSpeed = 0.5f;
+    private float dissolveTarget = 1f;
+
+    private float sliceAmount = 0f;
 
     public void SetActiveDustParticles(bool active)
     {
@@ -22,24 +29,47 @@ public class Mineral : MonoBehaviour
         gloomParticleGo.SetActive(active);
     }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log(collision.gameObject.name);   
-        if (other.gameObject.TryGetComponent(out ThirdPersonController _) && !RoverPocketStorage.Instance.IsHoldingMineral())
+        if (other.gameObject.TryGetComponent(out ThirdPersonController _) && !RoverPocketStorage.Instance.IsHoldingMineral() && !isDissolving)
         {
-            pickUpParticleEffect.Play();
+            isDissolving = true;
+            sliceAmount = 0f;
+            StartCoroutine(Dissolve());
 
+            // Remove this mineral from the manager
             MineralsFxManager.Instance.RemoveMineral(this);
 
+            // Set the picked mineral
             RoverPocketStorage.Instance.SetPickedMineral(mineralDataSO);
 
+            // Destroy this mineral after a delay
             Destroy(gameObject, mineralDestroyDelay);
         }
     }
-}
 
+    private IEnumerator Dissolve()
+    {
+        while (sliceAmount < dissolveTarget)
+        {
+            sliceAmount = Mathf.Lerp(sliceAmount, dissolveTarget, dissolveSpeed * Time.deltaTime);
+            dissolveMaterial.SetFloat("_SliceAmount", sliceAmount);
+            yield return null;
+        }
+
+        sliceAmount = dissolveTarget;
+        dissolveMaterial.SetFloat("_SliceAmount", sliceAmount);
+
+        isDissolving = false; // Reset the dissolution flag
+    }
+
+    // Ensure isDissolving is reset when the mineral is destroyed
+    private void OnDestroy()
+    {
+        // If the mineral is destroyed while dissolving, reset isDissolving
+        if (isDissolving)
+        {
+            isDissolving = false;
+        }
+    }
+}
