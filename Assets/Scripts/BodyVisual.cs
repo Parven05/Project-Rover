411262e7f1b849a815cae05464f5afafc2d2d4c6
Parven <parvendranofficial05@gmail.com>
2024-05-13 11:58:52 +0800

    added idle animation

--- Assets/Scripts/BodyVisual.cs
+++ Assets/Scripts/BodyVisual.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 public class BodyVisual : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
-
+    [SerializeField] private Animator headBounceAnim;
     [SerializeField] private ThirdPersonController thirdPersonController;
+
     private void Update()
     {
         if(thirdPersonController.IsRoverMoving())
         {
+            headBounceAnim.SetBool("isMove", false);
             transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            headBounceAnim.SetBool("isMove", true);
         }
     }
 }
